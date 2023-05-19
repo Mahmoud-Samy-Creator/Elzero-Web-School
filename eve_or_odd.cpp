262d3e44ae88f52193b3673963a6e67cd1d45031
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-19 12:07:54 +0300

    A problem shows if the input num is even or odd

---
+++ eve_or_odd.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int num;
+
+    cout << "=== Enter a number to check ===\n";
+    cout << "===== If it is even or odd =====\n";
+    cout << "==== Enter 0000 to stop ====\n";
+
+    cin >> num;
+
+    while(1)
+    {
+        
+        if (num % 2 == 0)
+        {
+            cout << "Number " << num << " is even\n";
+        }
+
+        else if(num % 2 != 0)
+        {
+            cout << "Number " << num << "is odd\n";
+        }
+
+        else
+        {
+            cout << "You did not entered a number";
+        }
+
+        cin >> num;
+    }
+}
