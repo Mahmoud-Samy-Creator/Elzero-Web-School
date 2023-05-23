a001c84dc46b5ee8b817a86b8eca6d1b3765b6d2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 20:57:09 +0300

    Assignment8

---
+++ Assignments/Lessons[16-23]/A8.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main()
+{
+    // Edit Anything Except Values
+    short a = 100;
+    int b = 15001500;
+    long double c = 100.54565746;
+
+// Do Not Edit
+    cout << sizeof(a) << " Bytes\n"; // 2 Bytes
+    cout << sizeof(b) << " Bytes\n"; // 8 Bytes
+    cout << sizeof(c) << " Bytes\n"; // 16 Bytes
+}
