5b09faf8c1da1d63c21a12cb9eb588713aa442f5
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 18:33:18 +0300

    Assignment4

---
+++ Assignments/Lessons[9-15]/A3.cpp
@@ -0,0 +1,16 @@
+/*
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ Not Valid ] => first character is a number ?
+  __name      [ Valid | Bad practice ] => Reason not efficient
+  name@name   [ Not Valid ] => Symbol @ ?
+  name10name  [ Valid | Bad ] => 
+  name!name   [ Valid | Bad ] => Reason ?
+  first_NAME  [ Valid | Good Practice ] => Reason ?
+  first_name  [ Valid | Good Practice ] => Reason ?
+  firstName   [ Valid | Bad Practice ] => Reason ?
+  first name  [ Not Valid ] => Reason ?
+  fn          [ Valid | Good Practice ] => Reason ?
+  public      [ Valid | Good Practice ] => Reason ?
+  Public      [ Valid | Good Practice ] => Reason ?
+*/
