e74f41ff7ae54e98792ac982dcd56168abcc3254
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 18:55:26 +0300

    Assignment9

---
+++ Assignments/Lessons[9-15]/A9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+    int num;
+    cin >> num;
+    cout << num * num;
+  // Num 10
+  // Needed Output 100 [10 * 10]
+  return 0;
+}
