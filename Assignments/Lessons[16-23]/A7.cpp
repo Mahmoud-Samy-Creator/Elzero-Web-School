527d005e17ac31637b92357759adc10e6879b2f9
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 20:54:28 +0300

    Assignment7

---
+++ Assignments/Lessons[16-23]/A7.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main()
+{
+    int a = 1;
+    int b = 13;
+    int c = 17;
+    int d = 70;
+
+    cout << char(d - a);
+    cout << char(d + c);
+    cout << char(d + b);
+
+// Output Needed
+//"EWS"
+}
