8a84e887c089af89d47cb12ddb7a34ad4f820046
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 20:38:03 +0300

    Assignment6

---
+++ Assignments/Lessons[16-23]/A6.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int main()
+{
+    int a = 69;
+    int b = 108;
+    int c = 122;
+    int d = 101;
+    int e = 114;
+    int f = 111;
+
+    cout << char(a) << endl;
+    cout << char(b) << endl;
+    cout << char(c) << endl;
+    cout << char(d) << endl;
+    cout << char(e) << endl;
+    cout << char(f) << endl;
+
+// Output Needed
+//"Character Of This ASCII Value 69 Is E"
+//"Character Of This ASCII Value 108 Is l"
+//"Character Of This ASCII Value 122 Is z"
+//"Character Of This ASCII Value 101 Is e"
+//"Character Of This ASCII Value 114 Is r"
+//"Character Of This ASCII Value 111 Is o"
+// Output Needed In Last Line
+//"Elzero"
+
+}
