e5b6b0292a1d2bd204343667684e38c7f51bc8e2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 18:20:51 +0300

    Assignment5

---
+++ Assignments/Lessons[1-8]/A5.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+// This Is My First File
+// In Learning Programming
+// Iam Happy <3
+// Iam Dragon
+
+#include <iostream> /* Preprocessor Directive */
+
+int main()
+{
+  std::cout; // Character Output << "Line One\n";
+  std::cout << "Line Two\n"; /* cout Function */
+  std::cout << "Line Three\n";
+  
+  return 0;
+} 
+/* End Main Function */
