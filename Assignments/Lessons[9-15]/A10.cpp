ce07c99ac3c8c5bf52abf17d640536f5d9d4f360
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 18:57:52 +0300

    Assignment10

---
+++ Assignments/Lessons[9-15]/A10.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+    short num1, num2;
+
+    cin >> num1;
+    cin >> num2;
+    cout << num1 * num2;
+  // Number One => 10
+  // Number Two => 100
+  // Needed Output 1000 [10 * 100]
+  return 0;
+}
