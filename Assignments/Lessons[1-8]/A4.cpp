cd8cf749f2a4ba9e7b86a908ff60abae65bafd15
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 18:13:52 +0300

    Assignment4

---
+++ Assignments/Lessons[1-8]/A4.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main()
+{
+    cout << "|=======================|" <<endl;
+    cout << "|== Elzero Web School ==|" << endl;
+    cout << "|==        C++        ==|" << endl;
+    cout << "|=======================|" <<endl;
+}
