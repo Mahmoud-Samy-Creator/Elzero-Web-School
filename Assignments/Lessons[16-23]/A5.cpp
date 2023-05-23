a8a8437b6b50e2bc8098cadac1a13fd1172aac74
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 20:29:38 +0300

    Assignment5

---
+++ Assignments/Lessons[16-23]/A5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a = '~';
+    char b = '&';
+    char c = '%';
+    char d = 'A';
+
+    cout << int(a) << endl;
+    cout << int(b) << endl;
+    cout << int(c) << endl;
+    cout << int(d) << endl;
+}
+
+
+// Output Needed
+/*
+"ASCII Value of ~ Is 126"
+"ASCII Value of & Is 38"
+"ASCII Value of % Is 37"
+"ASCII Value of A Is 65"
+*/
