9dbdf46ef007a4d5c0007926028dd65228cb4ad2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 20:26:36 +0300

    Assignment4

---
+++ Assignments/Lessons[16-23]/A4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Edit Line To Make 8 Become 4
+    cout << sizeof(10.5 + 5 + 20.5 + 10) << "\n"; // 8
+    cout << sizeof((int)(10.5 + 5 + 20.5 + 10)) << "\n"; // 8
+}
