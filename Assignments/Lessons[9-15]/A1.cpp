fd853306f59b329f86539503a4378d8e9f487c42
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 18:27:18 +0300

    Assignment1

---
+++ Assignments/Lessons[9-15]/A1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Write Your Code Here
+    short age = 40;
+    short period = 10;
+
+  // Do Not Edit The Next 2 Lines
+  cout << "My Age Is: " << age << "\n";
+  cout << period << " Years Ago My Age Was: " << age - period << "\n";
+  cout << "After" << period << "Years My Age Will Be " << age + period;
+
+  // Add The Third Line Below
+
+  return 0;
+}
