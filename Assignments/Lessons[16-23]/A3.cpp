407dc56d5b568add8d48f32389f03daa045bd74c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 20:22:52 +0300

    Assignment3

---
+++ Assignments/Lessons[16-23]/A3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+
+ cout <<"Maximum Integer Number Is => " << INT_MAX << endl;
+ cout <<"Minimum Integer Number Is => " << INT_MIN << endl;
+ cout <<"Minimum Integer Number Is => " << SHRT_MIN << endl;
+ cout <<"Minimum Integer Number Is => " << SHRT_MAX << endl;
+
+// Output Needed
+// "Maximum Integer Number Is => 2147483647"
+// "Minimum Integer Number Is => -2147483648"
+// "Maximum Short Integer Number Is => 32767"
+// "Minimum Short Integer Number Is => -32768"
+}
