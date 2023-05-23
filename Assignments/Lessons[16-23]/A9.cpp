b4d0d5998bb62a843e84a21dc247409867a855ca
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-23 21:00:44 +0300

    Assignment9

---
+++ Assignments/Lessons[16-23]/A9.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int main()
+{
+    // Edit Anything Except Values
+    short a = 100;
+    short b = -100;
+    short c = 100;
+    double d = 500.55;
+
+    // Do Not Edit
+    cout << a << "\n"; // 100
+    cout << b << "\n"; // -100
+    cout << c << "\n"; // 100
+    cout << d << "\n"; // 500.55
+}
