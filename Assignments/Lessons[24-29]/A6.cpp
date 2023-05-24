2350d646143d4d5f583d6f56228cc5241ef332c1
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-24 09:33:11 +0300

    Assignment6

---
+++ Assignments/Lessons[24-29]/A6.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int main()
+{
+    /*
+  We Need To Use This Operators In Solutions
+  - &&
+  - >=
+  - <=
+  - >
+  - <
+  - ==
+  - !
+  - ||
+*/
+
+int a = 10;
+int b = 25;
+int c = 15;
+
+cout << (a < b) << "\n";          // True
+cout << (c > a) << "\n";          // True
+cout << ((a + c) == b) << "\n";     // True
+cout << (c > a) << "\n";         // True
+cout << (c < b) << "\n";         // True
+cout << (b && a && b && c) << "\n"; // True
+cout << !(a == b) << "\n";         // True
+cout << (a && b || b && a) << "\n"; // True
+}
