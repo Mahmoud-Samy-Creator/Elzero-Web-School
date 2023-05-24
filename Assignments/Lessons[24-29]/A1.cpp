bad15fa357ef5949e8aa9ab1ffa5ca02791aaa9c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-24 09:07:24 +0300

    Assignment1

---
+++ Assignments/Lessons[24-29]/A1.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main()
+{
+    int num = 3;
+    cout << num + num << "\n";                 // 6
+    cout << num * num - num << "\n";           // 6
+    cout << (num * num) - num << "\n";         // 6
+    cout << (num * num / num) + num << "\n";   // 6
+    cout << (num + num) * (num / num) << "\n"; // 6
+    cout << (num / num) * (num + num) << "\n"; // 6
+    cout << (num + num) / (num / num) << "\n"; // 6
+}
