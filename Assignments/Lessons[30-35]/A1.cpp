24e97eb7f1d1734a0581ba4fdac20c08573f9d39
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-24 12:29:55 +0300

    Assignment1

---
+++ Assignments/Lessons[30-35]/A1.cpp
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int main()
+{
+    int age = 40;
+    int points = 800;
+    float rate = 8.5f;
+
+// Your Condition Here
+    if(age > 18 && points > 500 && rate > 5)
+        cout << "Yes Age > 18 & Points > 500 & Rate > 5";
+
+// Output Needed If All Conditions Is True
+//"Yes Age > 18 & Points > 500 & Rate > 5"
+}
