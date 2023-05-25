1aebe11a661cdbddb3396e65da71871e63f5ade8
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-25 10:19:11 +0300

    Assignment3

---
+++ Assignments/Lessons[36-37]/A3.cpp
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+int main()
+{
+  int num;
+  cin >> num;
+
+
+switch(num)
+{
+    case 10:
+    cout << "Case 1";
+    break;
+
+    case 20:
+    cout << "Case 2";
+    break;
+
+    case 30:
+    case 31:
+    case 32:
+    cout << "Case 3";
+    break;
+
+    default:
+    cout << "Invalid Number";
+}
+/*
+  if (num == 10)
+  {
+    cout << "Case 1";
+  }
+  else if (num > 19 && num < 21)
+  {
+    cout << "Case 2";
+  }
+  else if (num > 29 && num < 33)
+  {
+    cout << "Case 3";
+  }
+  else
+  {
+    cout << "Invalid Number";
+  }
+*/
+  return 0;
+}
