d72a611b621e5a561a5962e9d80f9a695eee05ee
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-28 15:57:51 +0300

    Assignment8

---
+++ Assignments/Lessons[38-46]/A8.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main()
+{
+
+    array <int, 3> nums{};
+    nums.fill(100);
+    cout << nums[0] << "\n"; // 100
+    cout << nums[1] << "\n"; // 100
+    cout << nums[2] << "\n"; // 100
+}
