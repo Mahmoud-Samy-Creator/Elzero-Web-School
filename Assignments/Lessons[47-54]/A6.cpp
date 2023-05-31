dad5960aa14b3005a56b4f28f5944bfab5f926b6
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-31 14:20:45 +0300

    Assignment 6

--- Assignments/Lessons[47-54]/A6.cpp
+++ Assignments/Lessons[47-54]/A6.cpp
@@ -3,10 +3,28 @@
 int main() {
     int i = 0, result = 10;
 
+    cout << "Using for loop" << endl;
+    cout << "================================\n";
+    
     for (i = 0 ; i < 5; i++)
     {
         cout << result << endl;
         result *= 10;
+    }
+
+    cout << endl;
+    
+    i = 0;
+    result = 10;
+
+    cout << "Using for loop" << endl;
+    cout << "================================\n";
+
+    while (i < 5)
+    {
+        cout << result << endl;
+        result *= 10;
+        i++;
     }
     return 0;
 }
