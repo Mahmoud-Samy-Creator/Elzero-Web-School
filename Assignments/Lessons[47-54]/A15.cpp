897b7af5e65867b951626fbb1d2120594dc2ac71
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-31 20:34:40 +0300

    Assignment 15

--- Assignments/Lessons[47-54]/A15.cpp
+++ Assignments/Lessons[47-54]/A15.cpp
@@ -7,31 +7,10 @@ int main() {
     
     for (i = 0 ; i <= help_num ; i++)
     {
-        if (i == 2)
-        {
-            cout << nums[2] << '+' << nums[2] 
-            << '=' << nums[2] + nums[2] << endl;
-            break;
-        }
-        else if (i > 2)
-        {
-            cout << nums[i] << '+' << nums[help_num - (help_num - j)] 
-            << '=' << nums[i] + nums[help_num - (help_num - j)] << endl;
+     int n = nums[i];
+     int opposite = nums[help_num - i];
 
-            j--;
-        }
-    
-        cout << nums[i+j] << '+' << nums[help_num - j] 
-        << '=' << nums[i+j] + nums[help_num - j] << endl;
-        j++;
-    }
-
-    j = 1;
-    for (i = 3 ; i <= help_num ; i++)
-    {
-        cout << nums[i] << '+' << nums[help_num - (help_num - j)] 
-        << '=' << nums[i] + nums[help_num - (help_num - j)] << endl;
-        j--;
+     cout << n << " + " << opposite << " = " << n + opposite << endl;   
     }
 
     return 0;
