3faf8d89b7febe3c012923342cbad899cb4625f7
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-05-31 14:14:44 +0300

    Assignment 5

---
+++ Assignments/Lessons[47-54]/A5.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+ 
+int main() {
+    short i  = 0, sum = 0;
+
+    for (i = 0 ; i < 10 ; i++)
+    {
+        cout << sum << endl;
+        sum += 3;
+    }
+    // Your code here
+    return 0;
+}
