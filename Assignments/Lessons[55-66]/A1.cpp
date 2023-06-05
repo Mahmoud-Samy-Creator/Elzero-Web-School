d38cd71fd1442c9362955fc7bbed8dd13e69b0a5
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-06-06 02:50:07 +0300

    Assignment 1

---
+++ Assignments/Lessons[55-66]/A1.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+ 
+int calcspecial(int a, int b)
+{
+    if (a == b)
+        return a+b;
+    
+    else if (a > b)
+        return a-b;
+    
+    else
+        return b-a;
+}
+
+int main() {
+    cout << calcspecial(40, 40) << "\n";    // First = Second -> 40 + 40 = 80
+    cout << calcspecial(200, 50) << "\n";   // First > Second -> 200 - 50 = 150
+    cout << calcspecial(100, 300) << "\n";  // First < Second -> 300 - 100 = 200
+  return 0;
+    // Your code here
+    return 0;
+}
