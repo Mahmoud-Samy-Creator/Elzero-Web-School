fe1d3a96cb89a5964dc5c0fc373472e979d07e22
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-06-06 02:50:53 +0300

    Custom Calculation Function

--- Assignments/Lessons[55-66]/A1.cpp
+++ Assignments/Lessons[55-66]/A1.cpp
@@ -16,7 +16,6 @@ int main() {
     cout << calcspecial(40, 40) << "\n";    // First = Second -> 40 + 40 = 80
     cout << calcspecial(200, 50) << "\n";   // First > Second -> 200 - 50 = 150
     cout << calcspecial(100, 300) << "\n";  // First < Second -> 300 - 100 = 200
-  return 0;
-    // Your code here
+
     return 0;
 }
