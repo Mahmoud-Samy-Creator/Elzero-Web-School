f4f15c8a91d2318d8c190323e970337eeccd786a
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-06-20 12:48:24 +0300

    Assignments

---
+++ Assignments/Lessons[67-74]/create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {1..18}
+do
+	touch A$i.cpp
+done
---
+++ Assignments/Lessons[67-74]/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#endif
