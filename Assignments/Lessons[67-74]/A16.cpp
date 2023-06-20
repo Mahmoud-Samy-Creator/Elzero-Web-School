f25fd7d68af3a673bb83616f81d4e2d395994c19
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-06-20 17:19:52 +0300

    Merge Vectors

---
+++ Assignments/Lessons[67-74]/A16.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  vector<int> n1 = { 1, 2, 3 };
+  vector<int> n2 = { 4, 5, 6 };
+  vector<int> allvectors(n1.size() + n2.size());
+  merge(n1.begin(), n1.end(), n2.begin(), n2.end(), allvectors.begin());
+
+  // Your Merge Code Here
+
+  for (int i : allvectors)
+  {
+    cout << i << "\n";
+  }
+  return 0;
+}
