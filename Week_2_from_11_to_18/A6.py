856d15c14456777653d784074f90339c01dd7747
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-10 20:17:50 +0300

    Add A6.py

---
+++ Week_2_from_11_to_18/A6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+num = "9"
+print(num.zfill(4))
+num = "15"
+print(num.zfill(4))
+num = "130"
+print(num.zfill(4))
+num = "950"
+print(num.zfill(4))
+num = "1500"
+print(num.zfill(4))
