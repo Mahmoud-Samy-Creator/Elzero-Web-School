50560202e8a56ee1cfb4219590d93e82eeb53974
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-10 20:18:40 +0300

    Add A11.py

--- Week_2_from_11_to_18/A11.py
+++ Week_2_from_11_to_18/A11.py
@@ -2,6 +2,6 @@
 
 msg = "I <3 Python And Although <3 Elzero Web School"
 
-print(msg.replace("<3", "Love"))
+print(msg.replace("<3", "Love", 1))
 # Needed Output
 # I Love Python And Although <3 Elzero Web School
