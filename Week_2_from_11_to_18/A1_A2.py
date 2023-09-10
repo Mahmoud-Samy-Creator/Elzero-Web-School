eb0e5cccdc993dd09431827d20109805bb1a3d29
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-10 18:40:40 +0300

    Add A1_A2.py

---
+++ Week_2_from_11_to_18/A1_A2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+name = "Osama"
+age = 38
+country = "Egypt"
+# Task 1
+print(f"Hello '{name}', How You Doing \\ \"\"\"" + f' Your age is "{age} ", ' + f'Your country is "{country}"')
+
+# Task 2
+print(f'Hello {name}, How You Doing \\ \n"""' + f' Your age is "{age}", ' + f'\nAnd Your country is: {country}')
