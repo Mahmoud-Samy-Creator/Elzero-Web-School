118fb95688d3e46ef9e2eae491b42b1087799fd2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-15 09:26:40 +0300

    Delete A2 and merge it in A1

--- Python_programming/Week_4_form_21_to_23/A2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
-
-print(friends[::2])
-print(friends[1::2])
-# Needed Output
-# "Osama", "Sayed", "Mahmoud"
-# "Ahmed", "Ali"
