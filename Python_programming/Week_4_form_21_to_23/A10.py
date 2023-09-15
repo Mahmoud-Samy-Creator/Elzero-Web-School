34508c4604ae881124568b579e8dffc749799efd
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-15 10:46:21 +0300

    A10

---
+++ Python_programming/Week_4_form_21_to_23/A10.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+technologies = ["Html", "CSS", "JS", "Python", ["Django", "Flask", "Web"]]
+print(technologies[-1][0])
+print(technologies[-1][2])
+# Needed Output
+# Django
+# Web
