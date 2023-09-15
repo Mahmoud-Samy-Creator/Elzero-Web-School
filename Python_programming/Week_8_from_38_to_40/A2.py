abdefe2760fd85598941d44e0e05f70b87ea01dd
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-15 20:35:33 +0300

    A2

---
+++ Python_programming/Week_8_from_38_to_40/A2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+age = int(input("What's your age?: "))
+
+if age > 16:
+    print(f"Hello Your Age Is {age}, All Articles Is Suitable For You")
+else:
+    print("Hello Your Age Is Under 16, Some Articles Is Not Suitable For You")
