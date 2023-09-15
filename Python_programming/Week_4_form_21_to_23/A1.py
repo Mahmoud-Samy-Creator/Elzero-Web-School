10367d5e9bfdb109fdf7169b0e4aad830333b9aa
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-15 09:23:28 +0300

    A1

---
+++ Python_programming/Week_4_form_21_to_23/A1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+
+print(friends[0])
+print(friends.pop(0))
+
+print(friends[-1])
+print(friends.pop(-1))
+
+
+# Needed Output
+# "Osama" => Method One
+# "Osama" => Method Two
+# "Mahmoud" => Method One
+# "Mahmoud" => Method Two
