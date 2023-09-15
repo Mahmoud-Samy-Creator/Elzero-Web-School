8eb22baaee30a529ed4db884690276d77dec9328
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-15 09:25:04 +0300

    A2

---
+++ Python_programming/Week_4_form_21_to_23/A2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
+
+print(friends[::2])
+print(friends[1::2])
+# Needed Output
+# "Osama", "Sayed", "Mahmoud"
+# "Ahmed", "Ali"
