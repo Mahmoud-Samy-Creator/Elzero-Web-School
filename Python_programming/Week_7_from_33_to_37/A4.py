11559562e73f8b7edb98b35b5dbdc3a730e44805
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-15 18:18:35 +0300

    A4

---
+++ Python_programming/Week_7_from_33_to_37/A4.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+num_one = 10
+num_two = 20
+
+result = num_two + num_one
+print(result)
+
+result_1 = result ** 3 
+print(result_1)
+
+result_2 = result_1 % 2600
+print(result_2)
+
+result_3 = result_2 / 5
+print(result_3)
+
+print(type(result_3))
+
+result_3 = str(result_3)
+
+print(type(result_3))
