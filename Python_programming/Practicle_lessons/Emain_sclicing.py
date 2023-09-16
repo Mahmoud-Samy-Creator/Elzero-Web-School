c8e1016605d8e07010dde2d8669093d320298cec
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-16 16:09:53 +0300

    Re correct names

--- Python_programming/Practicle_lessons/Emain_sclicing.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-name = input("What's your name: ")
-email = input("What's you email: ")
-
-user_name = email[:email.index("@")]
-domain = email[email.index("@") + 1 :]
-
-print(f"Your user name: {user_name}\nYour domain: {domain}")
