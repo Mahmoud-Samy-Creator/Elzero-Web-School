c7f554b524f8b284a9534aacdb06ed2debb0e36b
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-16 13:41:29 +0300

    A3.py

---
+++ Python_programming/Week_10_from_47_to_50/A3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# Code
+skills = ["HTML", "CSS", "JavaScript", "PHP", "Python"]
+
+while skills:
+# Type The Code Here in One Line
+    print(skills.pop(0))
+
+
+'''
+# Needed Output
+"HTML"
+"CSS"
+"JavaScript"
+"PHP"
+"Python"
+'''
