3c523119e1a33bc8463b726063f09cfd73b59d5f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-16 12:26:08 +0300

    Website list adder

--- Python_programming/Practicle_lessons/favourite_website.py
+++ Python_programming/Practicle_lessons/favourite_website.py
@@ -7,7 +7,7 @@ max_websites = 6
 while max_websites > 0:
     print(f"You have {max_websites} empty places in the stack")
     new_website = input(("Type your new favourite website without https://: ")).strip().lower()
-    fav_websites.append(new_website)
+    fav_websites.append(f"https://{new_website}")
     max_websites -= 1
 
 else:
