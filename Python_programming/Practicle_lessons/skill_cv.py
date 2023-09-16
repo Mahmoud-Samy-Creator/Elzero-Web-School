1b2bc7b80f677daf237cc48ffdaba6c7ad9b8cbf
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-16 14:22:10 +0300

    CLI simple skill cv

--- Python_programming/Practicle_lessons/skill_cv.py
+++ Python_programming/Practicle_lessons/skill_cv.py
@@ -7,5 +7,15 @@ skills = {
     "Python":"50%"
 }
 
+print("Printing skills with method [1]")
+print(40 * "-")
 for skill in skills:
     print(f"MY progress in lang {skill} => {skills[skill]}")
+
+print("\n")
+
+
+print("Printing skills with method [2]")
+print(40 * "-")
+for skill_key, skill_value in skills.items():
+    print(f"MY progress in lang {skill_key} => {skill_value}")
