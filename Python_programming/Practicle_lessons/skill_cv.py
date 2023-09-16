c8eb203f0f7ef13b74ad1019038b55c6dd639ebe
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-16 12:58:05 +0300

    simple cli skill cv

---
+++ Python_programming/Practicle_lessons/skill_cv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+skills = {
+    "HTML":"90%",
+    "CSS":"80%",
+    "JS":"70%",
+    "Python":"50%"
+}
+
+for skill in skills:
+    print(f"MY progress in lang {skill} => {skills[skill]}")
