b7d05f8cbe8441b1ef78d3088e57fd9ecfc31520
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-17 12:47:47 +0300

    A4

---
+++ Python_programming/Week_14_from_65_to_68/A4.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+#!/usr/bin/python3
+
+import os
+
+file_name = "textfile.txt"
+num = 0
+lines_num = 0
+word_num = 0
+char_num = 0
+letter_l_count = 0
+
+print(os.getcwd())
+print(os.path.dirname(os.path.abspath(__file__)))
+
+for i in range(1, 52):
+    file = open(f"text_{num}.txt", "w")
+    print(file.name)
+    num += 1
+    file.close()
+print(f"Number of files: {num - 1}")
+
+file = open("text1.txt", "w")
+file.write("Elzero Web School => 1\n")
+for i in range(0, 50):
+    file.write("Appended => Elzero Web School\n")
+file.close()
+
+file = open("text1.txt", "r")
+for line in file:
+    lines_num += 1
+    words = line.split()
+    word_num += len(words)
+    for word in words:
+        if 'l' in word:
+            letter_l_count += 1
+        char_num += len(word)
+
+print(f"Number of lines: {lines_num}")
+print(f"Number of words: {word_num}")
+print(f"Number of chars: {char_num}")
+print(f"Number of letter l: {letter_l_count}")
+
+for i in range(41, 51):
+    os.remove(f"text_{i}.txt")
