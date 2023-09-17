4a46aaec3e76f8714800b9dfcaff080df8082fa3
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-18 00:59:45 +0300

    A1.py

---
+++ Python_programming/Week_17_from_76_to_78/A1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import random
+
+print(f"Random number between 10 & 50: {random.randint(20, 50)}")
+print(f"Random number between 2 & 10: {random.randint(2, 10)}")
+print(f"Random number between 1 & 9: {random.randint(1, 9)}")
+
+print(dir(random))
