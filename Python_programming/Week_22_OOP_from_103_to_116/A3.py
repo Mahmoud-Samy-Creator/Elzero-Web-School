f77622281e48fc61454f29df42f9cbeeb3eeb747
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-09-19 18:46:03 +0300

    A3.py

---
+++ Python_programming/Week_22_OOP_from_103_to_116/A3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Message:
+    @staticmethod
+    def print_message():
+        return "Hello from Class Message"
+
+print(Message.print_message())
+
+# Output
+# Hello From Class Message
