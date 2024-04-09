1ecedaf0ccec6163bc1b5d5918ce6e638e08c464
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-09 20:07:43 +0200

    Lesson [6]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/06 Comments And Bad Practice ?.js
@@ -0,0 +1,21 @@
+// Wait The Window To Load
+window.onload = function () {
+    // Single Line Comment
+    document.querySelector("h1").style.color = "Blue"; // Single Line Comment
+  };
+  
+  // Single Line Comment
+  // Single Line Comment
+  // Single Line Comment
+  
+  /* Single Line Comment */
+  
+  /*
+    1
+    2
+    3
+    4
+    5
+  */
+  
+  // Ctrl + /
