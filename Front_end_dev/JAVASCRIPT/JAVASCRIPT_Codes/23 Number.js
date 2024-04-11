4a4ff43db0d08a7df0fd175c310f4d8b3e607331
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 05:40:03 +0200

    Lesson [23]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/23 Number.js
@@ -0,0 +1,23 @@
+/*
+  Number => It's an object & have methods for number manipulation & proprties
+  - Double Precision
+  - Syntactic Sugar "_"
+  - e
+  - **
+  - With Decimal
+  - Number + BigInt
+  - Number Min Value
+  - Number Max Value
+*/
+
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10 ** 6);
+console.log(10 * 10 * 10 * 10 * 10 * 10);
+console.log(1000000.0);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+// This is the max integer can be handled by the language
+// Regardless adding to it
+console.log(Number.MAX_VALUE + 2422162123)
