f50a1dfcc89b2df00dc1104f058bbb7c00bcba23
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 05:58:35 +0200

    Lesson [26] - Challenge

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/26 Number Challenge.js
@@ -0,0 +1,20 @@
+/*
+  Number Challenge
+*/
+
+let a = 100;
+let b = 2_00.5;
+let c = 1e2;
+let d = 2.4;
+
+// Find Smallest Number In All Variables And Return Integer
+console.log(parseInt(Math.min(a, b, c, d)));
+
+// Use Variables a + d One Time To Get The Needed Output
+console.log(a ** parseInt(d)); // 10000
+
+// Get Integer "2" From d Variable With 4 Methods
+console.log(d - 0.4);
+console.log(parseInt(d));
+console.log(Math.round(d));
+console.log(Math.floor(d));
