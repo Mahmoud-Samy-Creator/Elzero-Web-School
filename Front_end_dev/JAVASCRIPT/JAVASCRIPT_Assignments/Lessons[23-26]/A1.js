aa9ff3a9efdf725061305de2b479b370d1f03841
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-11 10:14:20 +0200

    A1

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[23-26]/A1.js
@@ -0,0 +1,15 @@
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(Math.round(100000.4)); // 100000
+console.log(Math.trunc(100000.99)); // 100000
+console.log(Math.ceil(99999.2)); // 100000
+console.log(Math.floor(100000.99)); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(Number(100000.4.toFixed(0))); // 100000
+console.log(10 ** 5); // 100000
+console.log(parseInt("100000.500 Mahmoud")); // 100000
+console.log(Math.round(parseFloat("100000.400 Mostafa"))); // 100000
+console.log(Math.max(99999, 8000, 100000, 50000)); // 100000
