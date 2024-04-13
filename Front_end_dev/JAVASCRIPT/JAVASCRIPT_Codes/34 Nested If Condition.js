e8fa846ccb8f0b74bfbab00c897dcfa8c21ce135
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-13 18:19:00 +0200

    Lesson [34]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/34 Nested If Condition.js
@@ -0,0 +1,33 @@
+/*
+  Nested If
+*/
+
+let price = 100;
+let discount = false;
+let discountAmount = 30;
+let country = "Egypt";
+let student = true;
+
+if (discount === true) {
+
+  price -= discountAmount;
+
+} else if (country === "Egypt") {
+
+  if (student === true) {
+
+    price -= discountAmount + 30;
+
+  } else {
+
+    price -= discountAmount + 10;
+
+  }
+
+} else {
+
+  price -= 10;
+
+}
+
+console.log(price);
