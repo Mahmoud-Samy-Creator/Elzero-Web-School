2609065c79f3df8a1d8662eba314c77668bff3e9
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-14 12:58:28 +0200

    A3

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[33-37]/A3.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 30;
+let num3 = "30";
+
+if (num3 > num1 && num3 !== num2) {
+  console.log("30 Is Larger Than 10 And Type string Not The Same Type As number")
+}
+if (num3 > num1 && num3 == num2 && num3 !== num2) {
+  console.log("30 Is Larger Than 10 And Value Is The Same As 30 And Type string Not The Same Type As number")
+}
+if (num3 !== num1 && num3 !== num2) {
+  console.log("{num3} Value And Type Is Not The Same As {num1} And Type Is Not The Same As {num2}")
+}
