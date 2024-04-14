4db88e8863898e3ede127ad97b79d3dff40d0e00
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-14 12:47:36 +0200

    A1

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[33-37]/A1.js
@@ -0,0 +1,17 @@
+// Test Case 1
+let num = 9;
+if (num < 10) {
+  console.log("00" + String(num))
+}
+
+// Test Case 2
+num = 20; // "020"
+if (num >  10 && num < 100) {
+  console.log("0" + String(num))
+}
+
+// Test Case 3
+num = 110; // "110"
+if (num >= 100) {
+  console.log(String(num))
+}
