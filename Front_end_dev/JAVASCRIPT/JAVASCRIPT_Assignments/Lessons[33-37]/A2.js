48c51a0bd9c3c114167b673483bb77d722a4f414
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-14 12:53:07 +0200

    A2

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[33-37]/A2.js
@@ -0,0 +1,16 @@
+let num1 = 9;
+let str = "9";
+let str2 = "20";
+
+if (num1 == str) {
+  console.log(`${num1} Is The Same Value As ${str}`)
+}
+if (num1 == str && num1 !== str) {
+  console.log(`${num1} Is The Same Value As ${str} But Not The Same Type"`)
+}
+if (num1 !== str2) {
+  console.log(`${num1} Is Not The Same Value Or The Same Type As ${str2}`)
+}
+if (typeof str == typeof str2 && str != str2) {
+  console.log(`${str} Is The Same Type As ${str2} But Not The Same Value`)
+} 
