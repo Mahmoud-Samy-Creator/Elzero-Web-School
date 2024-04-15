3678aeb255b79c876fca442a0b9b7cc276c01271
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-15 11:07:57 +0200

    Lesson [41]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/41 Using Length With Array.js
@@ -0,0 +1,16 @@
+/*
+  Array Methods
+  - Length
+*/
+
+// Index Start From 0 [ 0, 1, 2, 3, 4 ]
+
+let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa"];
+
+console.log(myFriends.length)
+
+
+// Cutting the array using length
+myFriends.length = 2
+
+console.log(myFriends)
