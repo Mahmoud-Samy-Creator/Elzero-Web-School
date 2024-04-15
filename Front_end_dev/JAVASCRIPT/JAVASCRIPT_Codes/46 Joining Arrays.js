6f214dc18e7fcf2ab510f6f8f14f104efc4bcc05
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-15 12:04:11 +0200

    Lesson [46]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/46 Joining Arrays.js
@@ -0,0 +1,17 @@
+/*
+  Arrays Methods [Joining]
+  - concat(array, array) => Return A New Array
+  - join(Separator)
+*/
+
+let myFriends = ["Ahmed", "Sayed", "Ali", "Osama", "Gamal", "Ameer"];
+let myNewFriends = ["Samar", "Sameh"];
+let schoolFriends = ["Haytham", "Shady"];
+
+let allFriends = myFriends.concat(myFriends, myNewFriends, schoolFriends)
+console.log(allFriends)
+
+console.log(allFriends.join())
+console.log(allFriends.join(" "))
+console.log(allFriends.join(" | "))
+console.log(allFriends.join(" @ "))
