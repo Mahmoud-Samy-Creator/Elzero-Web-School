384a77b0660c7a0209f945667370ff998b1cb574
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-15 21:37:16 +0200

    Lesson [53]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/53 Practice – Add Products To Page.js
@@ -0,0 +1,19 @@
+/*
+  Products Practice
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "iPhone"];
+let colors = ["Red", "Green", "Blue"];
+let showCount = 3;
+
+document.write(`<h1>Show ${showCount} Products</h1>`);
+
+for (let i = 0; i < showCount; i++) {
+  document.write(`<div>`);
+  document.write(`<h3>[${i + 1}] ${products[i]}</h3>`);
+  for (let j = 0; j < colors.length; j++) {
+    document.write(`<p>${colors[j]}</p>`);
+  }
+  document.write(`<p>${colors.join(" | ")}</p>`);
+  document.write(`</div>`);
+}
