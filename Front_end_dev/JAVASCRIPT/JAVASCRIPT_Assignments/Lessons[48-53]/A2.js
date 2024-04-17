d67cabc522dd6fc9286b8539f90ec01cff8b9a39
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-17 20:57:39 +0200

    A2

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[48-53]/A2.js
@@ -0,0 +1,19 @@
+let start = 10;
+let end = 0;
+let stop = 3;
+
+
+for (let i = start ; i >= 3 ; i--) {
+  if (i < 10) console.log(`0${i}`)
+  else console.log(i)
+}
+/*
+10
+09
+08
+07
+06
+05
+04
+03
+*/
