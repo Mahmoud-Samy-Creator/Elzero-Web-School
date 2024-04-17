99ffaee06cec03ce5fedd0d93d7aa0e72bf60bed
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-17 21:47:29 +0200

    Lesson 54

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/54 Loop – While.js
@@ -0,0 +1,13 @@
+/*
+  Loop
+  - While
+*/
+
+let products = ["Keyboard", "Mouse", "Pen", "Pad", "Monitor", "iPhone"];
+
+let index = 0;
+
+while (index < products.length) {
+  console.log(products[index]);
+  index += 1;
+}
