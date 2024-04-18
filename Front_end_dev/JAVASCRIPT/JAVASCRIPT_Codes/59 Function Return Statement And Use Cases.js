9d4545ed1d647ee720b44ae38ec8856617a3708c
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-18 02:48:26 +0200

    Lesson [60]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/59 Function Return Statement And Use Cases.js
@@ -0,0 +1,17 @@
+/*
+  Function
+  - Return
+  - Automatic Semicolon Insertion [No Line Terminator Allowed]
+  - Interrupting
+*/
+
+function generate(start, end) {
+  for (let i = start; i <= end; i++) {
+    if (i === 15) {
+      return `Interrupting`;
+    }
+    console.log(i);
+  }
+}
+
+generate(10, 20);
