4b5bb30d54d1ce83da8eeab7e5da856eb3ad274f
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-19 15:21:28 +0200

    A3

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[64-70]/A3.js
@@ -0,0 +1,20 @@
+// function checker(zName) {
+//   return function (status) {
+//     return function (salary) {
+//       return status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+//     };
+//   };
+// }
+
+// console.log(checker("Osama")("Available")(4000)); // Osama, My Salary Is 4000
+// console.log(checker("Ahmed")("Not Available")()); // Iam Not Avaialble  
+
+
+const checker = (zName) => (status) => (salary) =>
+  status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+
+console.log(checker("Osama")("Available")(4000));
+
+console.log(((zName, status, salary) => {
+  return "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+})("Osama", "Available", 4000));
