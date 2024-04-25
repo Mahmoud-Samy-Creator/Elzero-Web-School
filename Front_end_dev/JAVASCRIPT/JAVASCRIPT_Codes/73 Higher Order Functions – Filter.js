a0224a3b8a45ed7ef29bff8c97ca064504b2ffae
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-25 23:28:33 +0200

    Lesson [73]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/73 Higher Order Functions – Filter.js
@@ -0,0 +1,22 @@
+/*
+- Filter
+--- method creates a new array
+--- with all elements that pass the test implemented by the provided function.
+
+Syntax filter(callBackFunction(Element, Index, Array) { }, thisArg)
+- Element => The current element being processed in the array.
+- Index => The index of the current element being processed in the array.
+- Array => The Current Array
+*/
+
+// Get Friends With Name Starts With A
+let friends = ["Ahmed", "Sameh", "Sayed", "Asmaa", "Amgad", "Israa"];
+let A_Friends = friends.filter(ele => ele.startsWith('A'))
+
+console.log(A_Friends)
+
+// Get Even Numbers Only
+let numbers = [11, 20, 2, 5, 17, 10];
+
+let evenNums = numbers.filter(n => n %2 == 0)
+console.log(evenNums)
