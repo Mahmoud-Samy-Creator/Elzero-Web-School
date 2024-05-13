0dea65df1e08148bd3c6e82ff8676194f1215f48
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-05-14 00:29:37 +0300

    Lessons [102]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/102 What Is BOM ?.js
@@ -0,0 +1,21 @@
+/**
+ * BOM => [Browser Object Model]
+ * - Introduction
+ * - Window Object Is the Browser Window
+ * - Windows contains the document Object
+ * - All Global Variables and Objects and Functions Are Memebers of the window object
+ *  ------- Test Document & the console
+ * - What Can WE DO with the window object?
+ * --- Open window
+ * --- close window
+ * --- move window
+ * --- resize window
+ * --- print Document
+ * --- Run Code after a period of time once or more
+ * --- Fully control the URL
+ * --- Save Data Inside Browser to Use Late
+ */
+
+// window.console.log() === console.log()
+window.console.log("Hello Window")
+window.document.title = "Hello JS"
