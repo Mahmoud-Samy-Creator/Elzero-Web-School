d7154bcbe4b7743cb6a4f430d63de837b9833dcb
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-05-14 03:38:59 +0300

    Lessons [108]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/108 Window History Object.js
@@ -0,0 +1,16 @@
+/**
+ * BOM [Browser Object Model]
+ * - History API
+ * --- Properties
+ * ------ length
+ * ---- Methods
+ * ------- back()
+ * ------- forword()
+ * ------- go(Delta) => Position In History
+ */
+
+console.log(history)
+console.log(history.lenght)
+// console.log(history.lenght)
+history.back()
+history.forword()
