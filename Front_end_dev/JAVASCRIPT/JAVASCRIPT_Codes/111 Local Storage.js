e250d49cdda50a6687986e265f45708a28aa64c8
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-05-17 08:44:26 +0300

    Lessons [111]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/111 Local Storage.js
@@ -0,0 +1,40 @@
+/**
+ * BOM [Browser Object Model]
+ * Local Storage
+ * => It's a window property
+ * => Control the storage object
+ * => Methods to store, get & delete in the local storage
+ * - setItem
+ * - getItem
+ * - removeItem
+ * - clear => Delete all data in the local storage
+ * - key => Get the key value pair of data in local storage
+ */
+
+// It's a normal object
+console.log(typeof window.localStorage)
+
+// Set data in the local storage => We can user all ways of object
+// .setItem method (localStorage method)
+window.localStorage.setItem("color", "#F00")
+// object Property method 
+window.localStorage.fontWeight = "bold"
+// Key / value pair method
+window.localStorage["font-size"] = "20px"
+
+// Get objects
+console.log(window.localStorage.getItem("color"))
+console.log(window.localStorage.fontWeight)
+console.log(window.localStorage["font-size"])
+
+// USing local storage
+document.body.style.backgroundColor = window.localStorage["color"]
+
+// removing an item => using key
+window.localStorage.removeItem("color")
+
+// clear all
+// window.localStorage.clear()
+
+// key => behave like index
+console.log(window.localStorage.key(0))
