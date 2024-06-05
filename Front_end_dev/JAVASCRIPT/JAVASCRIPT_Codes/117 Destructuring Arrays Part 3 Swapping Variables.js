b3505eb1d998e0c9282e059624fb95d31b87ab13
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-05 17:55:02 +0300

    Lessons [117]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/117 Destructuring Arrays Part 3 Swapping Variables.js
@@ -0,0 +1,12 @@
+/**
+ * Destructuring
+ * - Destructuring Array => Swapping Variables
+ */
+
+let book = "Video";
+let video = "Book";
+
+[book, video] = [video, book];
+
+console.log(book)
+console.log(video)
