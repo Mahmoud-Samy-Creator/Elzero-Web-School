ec3489b3daa4bb6108f750152f8f53d95dd314d4
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-14 00:59:04 +0300

    A5

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[123-133]/A5.js
@@ -0,0 +1,7 @@
+let theName = "Elzero";
+console.log(Array.from(theName))
+console.log([...theName])
+console.log([...new Set(theName)])
+console.log(theName.split(""))
+console.log(Object.assign([], theName))
+
