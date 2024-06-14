d76a7abf58e25b0e4752d2dbad5939ed192a46ca
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-14 18:21:59 +0300

    Lessons [134]

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/134 Intro And What Is Regular Expression.js
@@ -0,0 +1,15 @@
+/**
+ * Reguler Expressions
+ * - Email
+ * - IP
+ * - Phone
+ * - URL
+ * It exists @ most of programming languages
+ */
+
+let str1 = '10 20 100 1000 5000';
+let str2 = 'Os1 Os12 Os123 Os1230s Os123120s123';
+let invalidEmail = 'Osama@@@gmail....com';
+let validMail = 'o@nn.sa';
+let ip = '192.168.2.1';
+let url = 'elzero.org';
