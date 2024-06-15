f471fd115df5da056bcf4aa0aa7be55fc6f6364e
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-15 22:20:46 +0300

    Lessons [153]

--- Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/153 Class Inheritance.js
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Codes/153 Class Inheritance.js
@@ -28,7 +28,7 @@ let adminOne = new Admin(110, "Mahmoud", 1);
 console.log(userOne.u); // Elzero
 console.log(userOne.sayHello()); // Hello Elzero
 console.log(adminOne.u); // Hello Mahmoud
-console.log("###");
+console.log("###");m
 console.log(adminOne.i); // 110
 console.log(adminOne.u); // Mahmoud
 console.log(adminOne.p); // 1
