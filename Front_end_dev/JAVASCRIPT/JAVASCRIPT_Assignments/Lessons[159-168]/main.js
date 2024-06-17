ab6a3211d12fb74095cddf6630fcef3bb5304dd9
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-17 23:47:44 +0300

    A8

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[159-168]/main.js
@@ -0,0 +1,3 @@
+import calc from './mode-one.js';
+import * as modOne from "./mode-two.js"
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[159-168]/mode-one.js
@@ -0,0 +1,3 @@
+export default function calc(numOne, numTwo, numThree) {
+    return numOne + numTwo + numThree;
+}
