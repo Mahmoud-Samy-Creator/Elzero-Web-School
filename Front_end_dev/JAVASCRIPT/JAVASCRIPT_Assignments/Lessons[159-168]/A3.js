de88e614cc3b73106de4fdd8cb84648fd06e219a
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-17 22:57:31 +0300

    A3

---
+++ Front_end_dev/JAVASCRIPT/JAVASCRIPT_Assignments/Lessons[159-168]/A3.js
@@ -0,0 +1,8 @@
+let date = new Date();
+
+console.log(date);
+console.log(date.getDay());
+console.log(date.getMonth());
+console.log(date.getFullYear());
+
+console.log(`Previous Month is ${date.getMonth() - 1} And Last Day ${date.getDay() - 1}`);
