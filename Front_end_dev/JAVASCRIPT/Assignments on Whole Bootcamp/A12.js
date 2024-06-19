898f7448ca270dffc84ec0a36f3274ba352c3f01
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-19 23:16:39 +0300

    A12

---
+++ Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A12.js
@@ -0,0 +1,10 @@
+let names = ["Osso", "Aola", "Essa", "Igaa", "Daad", "Roor"];
+let result = [];
+
+[a, b, , , c, d] = names;
+result.push(a);
+result.push(b);
+result.push(c);
+result.push(d);
+// Your Code Here
+console.log(result); // ['Osso', 'Aola', 'Daad', 'Roor']
