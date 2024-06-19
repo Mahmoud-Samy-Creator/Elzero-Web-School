3536b9dafddb967c3b9e0c7b04da1be3592d6cd4
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-19 23:19:31 +0300

    A12

--- Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A12.js
+++ Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A12.js
@@ -1,10 +1,14 @@
 let names = ["Osso", "Aola", "Essa", "Igaa", "Daad", "Roor"];
 let result = [];
 
-[a, b, , , c, d] = names;
-result.push(a);
-result.push(b);
-result.push(c);
-result.push(d);
+// [a, b, , , c, d] = names;
+// result.push(a);
+// result.push(b);
+// result.push(c);
+// result.push(d);
+
+for (let name of names) {
+    if (name[0].toLocaleLowerCase() === name[name.length - 1].toLocaleLowerCase()) result.push(name);
+}
 // Your Code Here
 console.log(result); // ['Osso', 'Aola', 'Daad', 'Roor']
