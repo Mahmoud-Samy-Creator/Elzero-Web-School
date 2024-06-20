4d29d344802d7952e691bd7f0a5276c9c04807b8
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-20 16:50:20 +0300

    A22

---
+++ Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A22.js
@@ -0,0 +1,7 @@
+let str1 = "AElzero";
+let str2 = "ZAcademy";
+
+let strOne = str1.split("").slice(1).join("");
+let strTwo = str2.replace(str2[0], ' ');
+let finalString = 
+console.log(strOne + strTwo); // Elzero Academy
