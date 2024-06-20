8c1841faf59398cdf1d7d9e37fc39c8b90819e93
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-20 16:36:50 +0300

    A20

---
+++ Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A20.js
@@ -0,0 +1,19 @@
+let st = "Elzero";
+
+console.log(st[st.length - 1]);
+console.log(`${st[st.length - 1]}`);
+console.log(st.charAt(st.length - 1));
+console.log(st.slice(-1));
+console.log(st.at(-1));
+console.log(st.substr(-1));
+console.log(st.substring(st.length - 1));
+console.log(st[st.indexOf('o')]);
+//
+//  Needed Output
+// "o"
+// "o"
+// "o"
+// "o"
+// "o"
+// "o"
+// "o"
