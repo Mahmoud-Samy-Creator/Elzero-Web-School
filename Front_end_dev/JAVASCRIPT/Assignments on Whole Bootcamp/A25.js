3e42c22a0b8a73894612d65fd8e987aa10429cc6
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-21 14:53:49 +0300

    A25

---
+++ Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A25.js
@@ -0,0 +1,19 @@
+let myArr = [10, 10, 20, 20, 10, 30, 50, 20, 10];
+
+// Method [1]
+// myArr = []
+
+// Method [2]
+// myArr.length = 0;
+
+
+// Method [3]
+// myArr.splice(0, myArr.length)
+
+// Method [4]
+
+// while(myArr.length) {
+//     myArr.pop();
+// }
+
+console.log(myArr)
