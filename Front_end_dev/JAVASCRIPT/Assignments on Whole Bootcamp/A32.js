b289f703492851f5a1e87c85fdf6dc313f3f6bd2
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-06-21 18:01:18 +0300

    A32

---
+++ Front_end_dev/JAVASCRIPT/Assignments on Whole Bootcamp/A32.js
@@ -0,0 +1,22 @@
+let strNumber = "10";
+
+// Method 1
+console.log(strNumber * 1); // 10
+
+// Method 2
+console.log(+strNumber);
+
+// Method 3
+console.log(Number(strNumber));
+
+// Method 4
+console.log(parseInt(strNumber));
+
+// Method 5
+console.log(parseFloat(strNumber));
+
+// Method 6
+console.log(Math.floor(strNumber));
+
+// Method 7
+console.log(Math.ceil(strNumber));
